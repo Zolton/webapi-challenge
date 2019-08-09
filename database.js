762ac2643d2776d456fb91748465a57f8c2b7260
Zolton <gmzesq@gmail.com>
2019-08-09 13:36:48 -0400

    CRUD works with array methods, but can't ID people by their object ID at all, keep getting errors

--- database.js
+++ database.js
@@ -1,32 +1,43 @@
+
 const array = [
   {
+    id: 1,
     name: "Frodo Baggins",
     chores: [
       {
         id: 1,
         description: "take the ring to Mordor",
         notes: "make your way to Mount Doom",
+        assignedTo: 1, // the id of Frodo,
+      completed: true
       },
       {
         id: 2,
         description: "destroy the ring",
         notes: "cast the ring into the fire inside Mount Doom",
+        assignedTo: 1,
+        completed: false
       }
     ]
   },
 
   {
+    id: 2,
     name: "Bilbo Baggins",
     chores: [
       {
         id: 1,
         description: "take out the garbage",
         notes: "Don't let it drip on the carpet",
+        assignedTo: 2,
+        completed: false
       },
       {
         id: 2,
         description: "Clean the shower",
         notes: "No more water stains",
+        assignedTo: 2,
+        completed: false
       }
     ]
   }
--- index.js
+++ index.js
@@ -3,22 +3,66 @@ const server = express();
 const array = require("./database");
 server.use(express.json());
 
+// function queryReader (req, res, next) {
+//     if (req.query.completed ===  true) {
+//         res.query.id.map(chores => chore = chores)
+//         res.status(200).json(chores)
+//     }
+// }
+
 server.get("/", (req, res) => {
-  console.log(array);
   res.status(200).json(array);
 });
 
 server.post("/", (req, res) => {
   newPerson = req.body;
+  newPerson.id = array.length + 1
   array.push(newPerson)
   res.status(200).json(newPerson);
 });
 
 server.delete('/:id', (req, res) => {
     const id = req.params.id
-    array.splice(id, 1)
-    res.status(200).json(array)
+    .then(
+    array.map(idFinder=>{
+        // Map over array, decide if the entered ID is equal or not
+        if (idFinder.id != id) {
+            // let foundID = id
+            // return foundID
+            res.status(404).json({Sorry: "That ID does not exist"})
+        }
+        else {
+            let foundID = id
+            array.splice(foundID, 1)
+            res.status(200).json(foundID)
+        }
+    }))
+
 })
+
+// server.get("/chores/:id", queryReader, (req, res)=>{
+//     // array.map(idFinder=>{
+//     //     if (idFinder.id != id) {
+//     //         console.log(id)
+//     //         res.status(404).json({Sorry: "That ID does not exist"})
+//     //     }
+//     //     else {
+//     //         let choresReturned = []
+//     //         res.status(200).json(choresReturned)
+//     //     }
+//     // })
+//     res.status(200).json({A: "ok"})
+// })
+
+// server.get("/chores/:id", (req, res)=>{
+//     //const replacement = req.body
+//     const id = req.params.id
+//     res.status(200).json(array[id].chores)
+// })
+
+// 1st parameter of splice: where to cut
+// 2nd: how much to cut
+// 3rd: what to put in the hole
 
 server.put('/:id', (req, res) => {
     const replacement = req.body
@@ -27,10 +71,11 @@ server.put('/:id', (req, res) => {
     res.status(200).json(array)
 })
 
-//months.splice(4, 1, 'May');
-// replaces 1 element at index 4
-////console.log(months);
-// expected output: Array ['Jan', 'Feb', 'March', 'April', 'May']
+// server.get("/:id", (req, res)=>{
+//     const id = req.params.id
+//     chores = array[id].chores
+// })
+
 
 server.listen(8000, () => {
   console.log("Server running on port 8000");
