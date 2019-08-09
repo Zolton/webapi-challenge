ba3dd8bb1f703f3ed4cd93a92e8f35e55f5d81ac
Zolton <gmzesq@gmail.com>
2019-08-09 14:29:19 -0400

    CRUD functionality for users is done.  Heroku should be working as well

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-require("dotenv").config()
-const port = process.env.PORT
+require("dotenv").config();
+const port = process.env.PORT;
 const express = require("express");
 const server = express();
 const array = require("./database");
 server.use(express.json());
-
-
 
 // function queryReader (req, res, next) {
 //     if (req.query.completed ===  true) {
@@ -20,29 +18,35 @@ server.get("/", (req, res) => {
 
 server.post("/", (req, res) => {
   newPerson = req.body;
-  newPerson.id = array.length + 1
-  array.push(newPerson)
+  newPerson.id = array.length + 1;
+  array.push(newPerson);
   res.status(200).json(newPerson);
 });
 
-server.delete('/:id', (req, res) => {
-    const id = req.params.id
-    .then(
-    array.map(idFinder=>{
-        // Map over array, decide if the entered ID is equal or not
-        if (idFinder.id != id) {
-            // let foundID = id
-            // return foundID
-            res.status(404).json({Sorry: "That ID does not exist"})
-        }
-        else {
-            let foundID = id
-            array.splice(foundID, 1)
-            res.status(200).json(foundID)
-        }
-    }))
+server.delete("/:id", (req, res) => {
+  let id = req.params.id;
+  const result = array.filter(test => test.id != id);
+  console.log(result);
+  //array.splice(id, 1);
+  
+  res.status(200).json(result);
+});
 
-})
+// 1st parameter of splice: where to cut
+// 2nd: how much to cut
+// 3rd: what to put in the hole
+
+server.put("/:id", (req, res) => {
+  const replacement = req.body;
+  const id = req.params.id;
+  array.splice(id, 1, { replacement });
+  res.status(200).json(array);
+});
+
+// server.get("/:id", (req, res)=>{
+//     const id = req.params.id
+//     chores = array[id].chores
+// })
 
 // server.get("/chores/:id", queryReader, (req, res)=>{
 //     // array.map(idFinder=>{
@@ -63,23 +67,6 @@ server.delete('/:id', (req, res) => {
 //     const id = req.params.id
 //     res.status(200).json(array[id].chores)
 // })
-
-// 1st parameter of splice: where to cut
-// 2nd: how much to cut
-// 3rd: what to put in the hole
-
-server.put('/:id', (req, res) => {
-    const replacement = req.body
-    const id = req.params.id
-    array.splice(id, 1, {replacement})
-    res.status(200).json(array)
-})
-
-// server.get("/:id", (req, res)=>{
-//     const id = req.params.id
-//     chores = array[id].chores
-// })
-
 
 server.listen(port, () => {
   console.log("Server running on port 8000");
