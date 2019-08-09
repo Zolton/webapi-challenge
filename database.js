2249186ae5d97ec531b46f75d88d373dde4d707a
Zolton <gmzesq@gmail.com>
2019-08-09 12:38:52 -0400

    Full CRUD functionality for users is done

--- database.js
+++ database.js
@@ -1,22 +1,35 @@
 const array = [
   {
-    id: 1,
     name: "Frodo Baggins",
     chores: [
       {
         id: 1,
         description: "take the ring to Mordor",
         notes: "make your way to Mount Doom",
-        assignedTo: 1, // the id of Frodo,
-        completed: true
       },
       {
         id: 2,
         description: "destroy the ring",
         notes: "cast the ring into the fire inside Mount Doom",
-        assignedTo: 1,
-        completed: false
+      }
+    ]
+  },
+
+  {
+    name: "Bilbo Baggins",
+    chores: [
+      {
+        id: 1,
+        description: "take out the garbage",
+        notes: "Don't let it drip on the carpet",
+      },
+      {
+        id: 2,
+        description: "Clean the shower",
+        notes: "No more water stains",
       }
     ]
   }
 ];
+
+module.exports = array
--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const express = require("express")
-const server = express()
-//const array = require("./database")
-server.use(express.json)
+const express = require("express");
+const server = express();
+const array = require("./database");
+server.use(express.json());
 
-server.get("/", (req, res)=>{
-    res.status(200).json({hello: "it works"})
+server.get("/", (req, res) => {
+  console.log(array);
+  res.status(200).json(array);
+});
+
+server.post("/", (req, res) => {
+  newPerson = req.body;
+  array.push(newPerson)
+  res.status(200).json(newPerson);
+});
+
+server.delete('/:id', (req, res) => {
+    const id = req.params.id
+    array.splice(id, 1)
+    res.status(200).json(array)
 })
 
-// server.get("/chores", (req, res)=>{
-//     array.chores.map(chore=>userChore = chore)
-//     res.status(200).json(userChore)
-// })
+server.put('/:id', (req, res) => {
+    const replacement = req.body
+    const id = req.params.id
+    array.splice(id, 1, {replacement})
+    res.status(200).json(array)
+})
 
-server.listen(8000, ()=>{console.log("Server running on port 8000")})
+//months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+////console.log(months);
+// expected output: Array ['Jan', 'Feb', 'March', 'April', 'May']
+
+server.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
